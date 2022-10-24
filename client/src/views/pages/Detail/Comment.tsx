4244a13e41d63896a61539a64c63e93681a967c6
lebathien91 <thienlb31dc18@st.buh.edu.vn>
2022-10-24 21:02:17 +0700

    fix

--- client/src/views/pages/Detail/Comment.tsx
+++ client/src/views/pages/Detail/Comment.tsx
@@ -1,7 +1,10 @@
+import { GlobalContext } from "@/store/GlobalState";
 import { FormSubmit } from "@/utils/interface";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 const Comment = () => {
+  const { state, dispatch } = useContext(GlobalContext);
+  const { user, token } = state.auth;
   const [content, setContent] = useState("");
 
   const handleSubmit = async (e: FormSubmit) => {
@@ -15,29 +18,30 @@ const Comment = () => {
       <div className="my-8">
         <form onSubmit={handleSubmit}>
           <div className="flex items-center">
-            <div className="w-16 h-16 mr-6">Avatar</div>
-            <div className="w-full">
-              <input
-                className="w-full pt-4 pb-2 focus:outline-none border-b border-gray-400 focus:border-gray-900"
-                placeholder="Viết bình luận..."
-                name="content"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-              <div className="mt-2 text-right">
-                <input
-                  className="px-4 py-2 bg-gray-200 rounded-sm text-black text-md font-semibold mr-4 cursor-pointer"
-                  type="button"
-                  onClick={() => setContent("")}
-                  value="Cancel"
-                />
-                <input
-                  className="px-4 py-2 bg-green-800 rounded-sm text-white text-md font-semibold"
-                  type="submit"
-                  value="Submit"
-                />
-              </div>
+            <div className="w-16 h-16 mr-4 overflow-hidden rounded-full">
+              <img src={user.avatar} />
             </div>
+
+            <input
+              className="flex-[1_1] pt-4 pb-2 focus:outline-none border-b border-gray-400 focus:border-gray-900"
+              placeholder="Viết bình luận..."
+              name="content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+          <div className="mt-2 text-right">
+            <input
+              className="px-4 py-2 bg-gray-200 rounded-sm text-black text-md font-semibold mr-4 cursor-pointer"
+              type="button"
+              onClick={() => setContent("")}
+              value="Cancel"
+            />
+            <input
+              className="px-4 py-2 bg-green-800 rounded-sm text-white text-md font-semibold"
+              type="submit"
+              value="Submit"
+            />
           </div>
         </form>
       </div>
